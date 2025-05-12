831ae3592245b36e9181d463dbf7a6206b9ee1e3
micheal-ndoh <michaelndoh9@gmail.com>
2025-05-12 08:12:39 +0100

    Added Task class to manage todo items with properties and methods

---
+++ Todo _list_project3/src/services/Task.java
@@ -0,0 +1,43 @@
+package services;
+
+public class Task {
+
+    private int id;
+    private String description;
+    private boolean completed;
+
+    public Task(int id, String description) {
+        this(id, description, false);
+    }
+
+    public Task(int id, String description, boolean completed) {
+        this.id = id;
+        this.description = description;
+        this.completed = completed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void markAsCompleted() {
+        this.completed = true;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{id=" + id + ", description='" + description + "', completed=" + completed + '}';
+    }
+}
