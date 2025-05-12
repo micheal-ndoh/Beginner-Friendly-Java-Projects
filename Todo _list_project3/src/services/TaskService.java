a5309dbc36f673f657dbe1d6a41555146009e808
micheal-ndoh <michaelndoh9@gmail.com>
2025-05-12 08:12:23 +0100

    Chore: added taskservice to help manage the todo system

---
+++ Todo _list_project3/src/services/TaskService.java
@@ -0,0 +1,39 @@
+package services;
+
+import java.util.List;
+import utils.FileHandler;
+
+public class TaskService {
+
+    private List<Task> tasks;
+
+    public TaskService() {
+        this.tasks = FileHandler.loadTasks(); 
+    }
+
+    public void addTask(String description) {
+        int id = tasks.size() + 1;
+        Task newTask = new Task(id, description);
+        tasks.add(newTask);
+        FileHandler.saveTasks(tasks);
+    }
+
+    public void deleteTask(int id) {
+        tasks.removeIf(task -> task.getId() == id);
+        FileHandler.saveTasks(tasks);
+    }
+
+    public void markTaskAsCompleted(int id) {
+        for (Task task : tasks) {
+            if (task.getId() == id) {
+                task.markAsCompleted();
+                break;
+            }
+        }
+        FileHandler.saveTasks(tasks);
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+}
