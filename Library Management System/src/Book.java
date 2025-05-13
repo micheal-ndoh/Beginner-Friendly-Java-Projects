2e6a3094983f16dc0f838beeed29b2b72eff7892
micheal-ndoh <michaelndoh9@gmail.com>
2025-05-13 04:05:17 +0100

    Add Book and LibraryManager classes for library management functionality including adding, removing, searching, listing, borrowing, and returning books

---
+++ Library Management System/src/Book.java
@@ -0,0 +1,42 @@
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private boolean isBorrowed;
+
+    public Book(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isBorrowed = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
+    public void borrow() {
+        this.isBorrowed = true;
+    }
+
+    public void returnBook() {
+        this.isBorrowed = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + id + "] " + title + " by " + author + (isBorrowed ? " (Borrowed)" : " (Available)");
+    }
+}
