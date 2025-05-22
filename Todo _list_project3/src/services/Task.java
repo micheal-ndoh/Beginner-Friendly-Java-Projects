0dde703edeaf81e2987a8256398ea5517fb3a2c2
micheal-ndoh <michaelndoh9@gmail.com>
2025-05-22 11:48:07 +0100

    Add setter method for task ID to allow modification of task identifiers

--- Todo _list_project3/src/services/Task.java
+++ Todo _list_project3/src/services/Task.java
@@ -20,6 +20,10 @@ public class Task {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getDescription() {
         return description;
     }
