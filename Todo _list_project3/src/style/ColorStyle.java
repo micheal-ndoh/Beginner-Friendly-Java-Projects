13a39f1f79e94d15cfa0cf6f641b84a7663965b3
micheal-ndoh <michaelndoh9@gmail.com>
2025-05-22 11:47:51 +0100

    Add ColorStyle class for terminal text styling

---
+++ Todo _list_project3/src/style/ColorStyle.java
@@ -0,0 +1,20 @@
+package style;
+
+public class ColorStyle {
+
+    public static class Color {
+
+        public static final String RED = "\u001B[31m";
+        public static final String GREEN = "\u001B[32m";
+        public static final String BLUE = "\u001B[34m";
+        public static final String YELLOW = "\u001B[33m";
+        public static final String PURPLE = "\u001B[35m";
+        public static final String RESET = "\u001B[0m";
+    }
+
+    public static class Style {
+
+        public static final String BOLD = "\u001B[1m";
+        public static final String RESET = "\u001B[0m";
+    }
+}
