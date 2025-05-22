f5659987fa1d3139f225b73117f2c071c7efd331
micheal-ndoh <michaelndoh9@gmail.com>
2025-05-22 16:00:09 +0100

    Add Subtraction class for BigInteger subtraction operations

---
+++ Calculator-project1/src/MathOperations/Subtraction.java
@@ -0,0 +1,9 @@
+package MathOperations;
+
+import java.math.BigInteger;
+
+public class Subtraction {
+    public BigInteger subtract(BigInteger a, BigInteger b) {
+        return a.subtract(b);
+    }
+}
