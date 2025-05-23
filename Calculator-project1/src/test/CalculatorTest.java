38f7706b5083e0103c67143213bc05f46e5698e8
micheal-ndoh <michaelndoh9@gmail.com>
2025-05-23 15:50:39 +0100

    Comment out the CalculatorTest class and its methods for future reference

--- Calculator-project1/src/test/CalculatorTest.java
+++ Calculator-project1/src/test/CalculatorTest.java
@@ -1,58 +1,58 @@
-package MathOperations;
+// package MathOperations;
 
-import main.java.com.MathOperations.*;
+// import main.java.com.MathOperations.*;
 
-import org.junit.jupiter.api.Test;
-import java.math.BigInteger;
-import static org.junit.jupiter.api.Assertions.*;
+// import org.junit.jupiter.api.Test;
+// import java.math.BigInteger;
+// import static org.junit.jupiter.api.Assertions.*;
 
-public class CalculatorTest {
+// public class CalculatorTest {
 
-    @Test
-    public void testAddition() {
-        Addition addition = new Addition();
-        assertEquals(new BigInteger("15"), addition.add(new BigInteger("10"), new BigInteger("5")));
-    }
+//     @Test
+//     public void testAddition() {
+//         Addition addition = new Addition();
+//         assertEquals(new BigInteger("15"), addition.add(new BigInteger("10"), new BigInteger("5")));
+//     }
 
-    @Test
-    public void testSubtraction() {
-        Subtraction subtraction = new Subtraction();
-        assertEquals(new BigInteger("5"), subtraction.subtract(new BigInteger("10"), new BigInteger("5")));
-    }
+//     @Test
+//     public void testSubtraction() {
+//         Subtraction subtraction = new Subtraction();
+//         assertEquals(new BigInteger("5"), subtraction.subtract(new BigInteger("10"), new BigInteger("5")));
+//     }
 
-    @Test
-    public void testMultiplication() {
-        Multiply multiply = new Multiply();
-        assertEquals(new BigInteger("50"), multiply.multiply(new BigInteger("10"), new BigInteger("5")));
-    }
+//     @Test
+//     public void testMultiplication() {
+//         Multiply multiply = new Multiply();
+//         assertEquals(new BigInteger("50"), multiply.multiply(new BigInteger("10"), new BigInteger("5")));
+//     }
 
-    @Test
-    public void testDivision() {
-        Divide divide = new Divide();
-        assertEquals(new BigInteger("2"), divide.divide(new BigInteger("10"), new BigInteger("5")));
-    }
+//     @Test
+//     public void testDivision() {
+//         Divide divide = new Divide();
+//         assertEquals(new BigInteger("2"), divide.divide(new BigInteger("10"), new BigInteger("5")));
+//     }
 
-    @Test
-    public void testFactorial() {
-        Factorial factorial = new Factorial();
-        assertEquals(new BigInteger("120"), factorial.fact(5));
-    }
+//     @Test
+//     public void testFactorial() {
+//         Factorial factorial = new Factorial();
+//         assertEquals(new BigInteger("120"), factorial.fact(5));
+//     }
 
-    @Test
-    public void testFactorialOfZero() {
-        Factorial factorial = new Factorial();
-        assertEquals(BigInteger.ONE, factorial.fact(0));
-    }
+//     @Test
+//     public void testFactorialOfZero() {
+//         Factorial factorial = new Factorial();
+//         assertEquals(BigInteger.ONE, factorial.fact(0));
+//     }
 
-    @Test
-    public void testFactorialNegativeNumber() {
-        Factorial factorial = new Factorial();
-        assertThrows(IllegalArgumentException.class, () -> factorial.fact(-1));
-    }
+//     @Test
+//     public void testFactorialNegativeNumber() {
+//         Factorial factorial = new Factorial();
+//         assertThrows(IllegalArgumentException.class, () -> factorial.fact(-1));
+//     }
 
-    @Test
-    public void testDivideByZero() {
-        Divide divide = new Divide();
-        assertThrows(ArithmeticException.class, () -> divide.divide(new BigInteger("10"), BigInteger.ZERO));
-    }
-}
+//     @Test
+//     public void testDivideByZero() {
+//         Divide divide = new Divide();
+//         assertThrows(ArithmeticException.class, () -> divide.divide(new BigInteger("10"), BigInteger.ZERO));
+//     }
+// }
