29903db7b31dc6a27e083be8b38ee82f05935e36
micheal-ndoh <michaelndoh9@gmail.com>
2025-05-23 11:07:02 +0100

    Enhance Rock-Paper-Scissors game to include Lizard and Spock options, updating game logic and user prompts accordingly.

--- Rock_game/src/RockPaperScissor.java
+++ Rock_game/src/RockPaperScissor.java
@@ -9,11 +9,11 @@ public class RockPaperScissor {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        String[] options = {"rock", "paper", "scissors"};
-        System.out.println("Welcome to Rock-Paper-Scissors game");
+        String[] options = {"rock", "paper", "scissors", "lizard", "spock"};
+        System.out.println("Welcome to Rock-Paper-Scissors-Lizard-Spock game");
 
         while (true) {
-            System.out.print("Enter your move (rock, paper, scissors or quit): ");
+            System.out.print("Enter your move (rock, paper, scissors, lizard, spock or quit): ");
             String input = scanner.nextLine().toLowerCase();
 
             if (input.equals("quit")) {
@@ -26,14 +26,16 @@ public class RockPaperScissor {
                 continue;
             }
 
-            String player2Move = options[random.nextInt(3)];
+            String player2Move = options[random.nextInt(options.length)];
             System.out.println("Player2 chose: " + player2Move);
 
             if (input.equals(player2Move)) {
                 System.out.println("Draw");
-            } else if ((input.equals("rock") && player2Move.equals("scissors"))
-                    || (input.equals("paper") && player2Move.equals("rock"))
-                    || (input.equals("scissors") && player2Move.equals("paper"))) {
+            } else if ((input.equals("rock") && (player2Move.equals("scissors") || player2Move.equals("lizard")))
+                    || (input.equals("paper") && (player2Move.equals("rock") || player2Move.equals("spock")))
+                    || (input.equals("scissors") && (player2Move.equals("paper") || player2Move.equals("lizard")))
+                    || (input.equals("lizard") && (player2Move.equals("spock") || player2Move.equals("paper")))
+                    || (input.equals("spock") && (player2Move.equals("scissors") || player2Move.equals("rock")))) {
                 System.out.println("You win");
             } else {
                 System.out.println("Player2 wins");
